964f135b4fb7c3270777e180e6a03d2b9de6e890
lecaophuongthuy <lecaophuongthuy1997@gmail.com>
2022-01-22 02:26:10 +0900

    Single Page Applications Part 1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import  { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle} from 'reactstrap';
+
+function ReanderCard({item}) {
+    return(
+        <Card>
+            <CardImg src={item.image} alt={item.name} />
+            <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null } 
+                <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
 
 function Home(props) {
     return(
         <div className='container'>
-            <h4>Home</h4>
+            <div className='row align-items-start'>
+                <div className='col-12 col-md m-1'>
+                    <ReanderCard item={props.dish} />
+                </div>
+                <div className='col-12 col-md m-1'>
+                    <ReanderCard item={props.promotion} />
+                </div>
+                <div className='col-12 col-md m-1'>
+                    <ReanderCard item={props.leader} />
+                </div>
+            </div>
         </div>
     );
 }
