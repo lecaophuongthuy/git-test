bb4b53634c8a4c4366a48f2231a73fcae3e0dd8c
lecaophuongthuy <lecaophuongthuy1997@gmail.com>
2022-02-17 23:59:11 +0900

    commit 2

--- src/components/BangluongComponent.js
+++ src/components/BangluongComponent.js
@@ -2,15 +2,35 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay,CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
 import { Link} from 'react-router-dom';
 
+function RenderBangluong({staff}) {
+    return (
+        <Card></Card>
+    )
+}
+
+
+
 const Bangluong = (props) => {
-   
+    const bangluong = props.staffs.map((staff) => {
+        return (
+            <div key={staff.id} className='col-xs-12 col-md-6 col-lg-4'>
+               
+                <RenderBangluong staff={staff}>
+               </RenderBangluong>
+              
+               
+            </div>
+        )
+    });
     return (
         <div className='container'>
-        
+            <div className='row'>
+
+                    {bangluong}
+            </div>
         </div>
 
     );
-
-}
+    }
       
 export default Bangluong;
