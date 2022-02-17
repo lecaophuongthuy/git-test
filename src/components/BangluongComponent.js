b5a9e189b791e2c99055be48deae0b67d7f99806
lecaophuongthuy <lecaophuongthuy1997@gmail.com>
2022-02-18 01:56:05 +0900

    commit 5

--- src/components/BangluongComponent.js
+++ src/components/BangluongComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay,CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import { Card, CardImg, CardBody, CardImgOverlay,CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
 import { Link} from 'react-router-dom';
 
 function RenderBangluong({staff}) {
@@ -9,9 +9,13 @@ function RenderBangluong({staff}) {
             <CardTitle>
                <p style={{fontWeight:"bold",fontSize:30}}>{staff.name}</p> 
             </CardTitle>
+            <CardBody>
             <p> Mã nhân viên: {staff.id}</p>
             <p> Hệ số lương: {Math.round(staff.salaryScale)}</p>
-            <p></p>
+            <p> Số giờ làm thêm: {staff. overTime}</p>
+            <div style={{backgroundColor:"#d6d4d8",width:250,marginLeft:8}}>
+                 Lương:{Math.round((Math.round(staff.salaryScale) * 3000000) + staff.overTime * 200000)} </div>
+            </CardBody>
         </Card>
     
     )
