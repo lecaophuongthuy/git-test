a5d38e49884fbc4ed1e77d1789914cbd0fbba691
lecaophuongthuy <lecaophuongthuy1997@gmail.com>
2022-02-18 02:45:27 +0900

    commit 7

--- src/components/BangluongComponent.js
+++ src/components/BangluongComponent.js
@@ -4,8 +4,8 @@ import { Link} from 'react-router-dom';
 
 function RenderBangluong({staff}) {
     return (
-    
-            <Card  style={{marginBottom:8,marginTop:8}}>
+    <div >
+        <Card style={{marginBottom:8,marginTop:8}}>
             <CardTitle>
                <p style={{fontWeight:"bold",fontSize:30}}>{staff.name}</p> 
             </CardTitle>
@@ -13,10 +13,12 @@ function RenderBangluong({staff}) {
             <p> Mã nhân viên: {staff.id}</p>
             <p> Hệ số lương: {Math.round(staff.salaryScale)}</p>
             <p> Số giờ làm thêm: {staff. overTime}</p>
-            <div style={{backgroundColor:"#d6d4d8",width:250,marginLeft:8}}>
+            <div style={{backgroundColor:"#d6d4d8",width:150,marginLeft:8}}>
                  Lương:{Math.round((Math.round(staff.salaryScale) * 3000000) + staff.overTime * 200000)} </div>
             </CardBody>
         </Card>
+    </div>
+           
     
     )
 }
@@ -26,23 +28,26 @@ function RenderBangluong({staff}) {
 const Bangluong = (props) => {
     const bangluong = props.staffs.map((staff) => {
         return (
-            <div key={staff.id} className='col-xs-12 col-md-6 col-lg-4'>
-               
-                <RenderBangluong staff={staff}>
+            <div className="'col-xs-12 col-md-6 col-lg-4'">
+               <RenderBangluong staff={staff}>
                </RenderBangluong>
-              
-               
-            </div>
+               </div>
         )
     });
     return (
         <div className='container'>
-            <div className='row'>
-
+                 <div className='row'>
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/staffs'>Nhân Viên</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Bảng Lương</BreadcrumbItem>
+                    </Breadcrumb>                  
+                </div >
+                <div className='row'>
                     {bangluong}
-            </div>
-        </div>
-
+                </div>
+              
+             </div>
+            
     );
     }
       
