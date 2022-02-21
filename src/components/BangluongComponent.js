0dc944a0859e46df5ef1b70156fd3e71b0e93d38
lecaophuongthuy <lecaophuongthuy1997@gmail.com>
2022-02-22 01:12:17 +0900

    commit 10

--- src/components/BangluongComponent.js
+++ src/components/BangluongComponent.js
@@ -1,54 +1,61 @@
-import React from 'react';
-import { Card,CardBody,CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
-import { Link} from 'react-router-dom';
+import React from "react";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
-function RenderBangluong({staff}) {
-    return (
-    <div >
-        <Card style={{marginBottom:8,marginTop:8}}>
-            <CardTitle>
-               <p style={{fontWeight:"bold",fontSize:30}}>{staff.name}</p> 
-            </CardTitle>
-            <CardBody>
-                <p> Mã nhân viên: {staff.id}</p>
-                <p> Hệ số lương: {Math.round(staff.salaryScale)}</p>
-                <p> Số giờ làm thêm: {staff. overTime}</p>
-                <div style={{backgroundColor:"#D3D3D3"}}>
-                <p style={{left:15}}> Lương:{Math.round((Math.round(staff.salaryScale) 
-                * 3000000) + staff.overTime * 200000)} </p>
-                </div>
-            </CardBody>
-        </Card>
+function RenderBangluong({ staff }) {
+  return (
+    <div>
+      <Card style={{ marginBottom: 8, marginTop: 8 }}>
+        <CardTitle>
+          <p style={{ fontWeight: "bold", fontSize: 30 }}>{staff.name}</p>
+        </CardTitle>
+        <CardBody>
+          <p> Mã nhân viên: {staff.id}</p>
+          <p> Hệ số lương: {Math.round(staff.salaryScale)}</p>
+          <p> Số giờ làm thêm: {staff.overTime}</p>
+          <div style={{ backgroundColor: "#D3D3D3" }}>
+            <p style={{ left: 15 }}>
+              {" "}
+              Lương:
+              {Math.round(
+                Math.round(staff.salaryScale) * 3000000 +
+                  staff.overTime * 200000
+              )}{" "}
+            </p>
+          </div>
+        </CardBody>
+      </Card>
     </div>
-           
-    
-    )
+  );
 }
 
 const Bangluong = (props) => {
-    const bangluong = props.staffs.map((staff) => {
-        return (
-            <div className="col-xs-12 col-md-6 col-lg-4">
-               <RenderBangluong staff={staff}>
-               </RenderBangluong>
-               </div>
-        )
-    });
+  const bangluong = props.staffs.map((staff) => {
     return (
-        <div className='container'>
-                 <div className='row'>
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to='/staffs'>Nhân Viên</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>Bảng Lương</BreadcrumbItem>
-                    </Breadcrumb>                  
-                </div >
-                <div className='row'>
-                    {bangluong}
-                </div>
-              
-             </div>
-            
+      <div className="col-xs-12 col-md-6 col-lg-4">
+        <RenderBangluong staff={staff}></RenderBangluong>
+      </div>
     );
-    }
-      
+  });
+  return (
+    <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/staffs">Nhân Viên</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>Bảng Lương</BreadcrumbItem>
+        </Breadcrumb>
+      </div>
+      <div className="row">{bangluong}</div>
+    </div>
+  );
+};
+
 export default Bangluong;
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,66 +1,71 @@
-import React, {Component} from 'react';
-import {Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem} from 'reactstrap';
-import { NavLink} from 'react-router-dom';
+import React, { Component } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      isNavOpen: false,
+    };
+    this.toggleNav = this.toggleNav.bind(this);
+  }
 
-        this.state={
-            isNavOpen : false
-        };
-        this.toggleNav = this.toggleNav.bind(this);
-    }
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen,
+    });
+  }
 
-    toggleNav() {
-        this.setState({
-            isNavOpen : !this.state.isNavOpen
-        })
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-            <Navbar dark expand="md">
-               <div className="container">
-                <NavbarToggler onClick={this.toggleNav} />
-                 <NavbarBrand className="mr-auto" href="/">
-                   <img src="assets/images/logo.png" height="30" width="41"
-                    alt="Ristorante Con Fusion"/>
-                 </NavbarBrand>
-                 <Collapse isOpen={this.state.isNavOpen} navbar>
-                 <Nav navbar>
-                     <NavItem>
-                         <NavLink className="nav-link" to="/staffs">
-                         <i class="fa fa-user" aria-hidden="true"></i>
-                           Nhân viên
-                         </NavLink>
-                     </NavItem>
-                     <NavItem>
-                         <NavLink className="nav-link" to="/phongban">
-                         <i class="fa fa-address-card-o" aria-hidden="true"></i>
-                            Phòng ban
-                         </NavLink>
-                     </NavItem>
-                     <NavItem>
-                       
-                         <NavLink className="nav-link" to="/bangluong">
-                         <i class="fa fa-money" aria-hidden="true"></i>
-                            Bảng lương
-                         </NavLink>
-                     </NavItem>
-                 </Nav>
-                 </Collapse>
-
-               </div>
-             </Navbar>
-           
-            
-           </React.Fragment>
-        );
-   
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar dark expand="md">
+          <div className="container">
+            <NavbarToggler onClick={this.toggleNav} />
+            <NavbarBrand className="mr-auto" href="/">
+              <img
+                src="assets/images/logo.png"
+                height="30"
+                width="41"
+                alt="Ristorante Con Fusion"
+              />
+            </NavbarBrand>
+            <Collapse isOpen={this.state.isNavOpen} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink className="nav-link" to="/staffs">
+                    <i class="fa fa-user" aria-hidden="true"></i>
+                    Nhân viên
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/phongban">
+                    <i class="fa fa-address-card-o" aria-hidden="true"></i>
+                    Phòng ban
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/bangluong">
+                    <i class="fa fa-money" aria-hidden="true"></i>
+                    Bảng lương
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </div>
+        </Navbar>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Header;
